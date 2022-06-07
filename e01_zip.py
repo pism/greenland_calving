b9c7692c4d9d106343a72c63d0dd2c561d36fcc2
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2022-06-06 18:21:22 -0800

    Markdown fixes

--- e01_zip.py
+++ e01_zip.py
@@ -1,6 +1,75 @@
 from uafgi import gitutil,ioutil
 import os,subprocess,zipfile
 import uafgi.data
+import re
+import subprocess
+import sys
+
+import yaml
+
+
+# =====================================================================
+
+# https://gist.githubusercontent.com/gwerbin/dab3cf5f8db07611c6e0aeec177916d8/raw/63c7191731b530aceaf887e9f33478fcad33d351/conda_env_export.py
+"""
+Export a Conda environment with --from-history, but also append
+Pip-installed dependencies
+
+Exports only manually-installed dependencies, excluding build versions, but
+including Pip-installed dependencies.
+
+Lots of issues requesting this functionality in the Conda issue tracker, no
+sign of progress (as of March 2020).
+
+TODO (?): support command-line flags -n and -p
+"""
+
+
+def export_env(history_only=False, include_builds=False):
+    """ Capture `conda env export` output """
+    cmd = ['conda', 'env', 'export']
+    if history_only:
+        cmd.append('--from-history')
+        if include_builds:
+            raise ValueError('Cannot include build versions with "from history" mode')
+    if not include_builds:
+        cmd.append('--no-builds')
+    cp = subprocess.run(cmd, stdout=subprocess.PIPE)
+    try:
+        cp.check_returncode()
+    except:
+        raise
+    else:
+        return yaml.safe_load(cp.stdout)
+
+
+def _is_history_dep(d, history_deps):
+    if not isinstance(d, str):
+        return False
+    d_prefix = re.sub(r'=.*', '', d)
+    return d_prefix in history_deps
+
+
+def _get_pip_deps(full_deps):
+    for dep in full_deps:
+        if isinstance(dep, dict) and 'pip' in dep:
+            return dep
+
+
+def _combine_env_data(env_data_full, env_data_hist):
+    deps_full = env_data_full['dependencies']
+    deps_hist = env_data_hist['dependencies']
+    deps = [dep for dep in deps_full if _is_history_dep(dep, deps_hist)]
+
+    pip_deps = _get_pip_deps(deps_full)
+
+    env_data = {}
+    env_data['channels'] = env_data_full['channels']
+    env_data['dependencies'] = deps
+    env_data['dependencies'].append(pip_deps)
+
+    return env_data
+# =====================================================================
 
 harness_root = gitutil.harness_root('.')
 odir = uafgi.data.join_outputs('zips')
@@ -11,6 +80,8 @@ def zip_results():
     with zipfile.ZipFile(
         os.path.join(odir, '1_greenland_calving_results.zip'),
         mode='w') as ozip:
+
+        ozip.write('README.md')
 
         with ioutil.pushd(harness_root):
             ozip.write('greenland_calving/outputs/stability/greenland_calving.csv')
@@ -25,6 +96,8 @@ def zip_data():
         os.path.join(odir, '2_greenland_calving_data.zip'),
         mode='w') as ozip:
 
+
+        ozip.write('README.md')
 
         # Add Data Files
         for arcdir in [
@@ -44,6 +117,8 @@ def zip_code():
     with zipfile.ZipFile(
         os.path.join(odir, '3_greenland_calving_code.zip'),
         mode='w') as ozip:
+
+        ozip.write('README.md')
 
         # Add Source Files
         for repo in ('greenland_calving', 'uafgi'):
@@ -87,6 +162,8 @@ def zip_sigmas():
 
     with zipfile.ZipFile(ofname, mode='w') as ozip:
 
+        ozip.write('README.md')
+
         paths = ['greenland_calving', 'outputs', 'itslive', 'sigma']
         dir = os.path.join(harness_root, *paths)
         arcdir = os.path.join(*paths)
@@ -100,6 +177,7 @@ def zip_sigmas():
 
 def main():
     zip_results()
+    zip_data()
     zip_code()
     zip_sigmas()
 
